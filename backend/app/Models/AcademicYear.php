1ff7a9151c06f0f83fb2f3dcb2b6d22a6aea47c2
Ilyes Azzi <ilyes24.azzi@gmail.com>
2023-09-03 19:41:09 +0200

    Backend - Calculate year academic days available implmented

--- backend/app/Models/AcademicYear.php
+++ backend/app/Models/AcademicYear.php
@@ -4,10 +4,13 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Carbon\Carbon;
 
 class AcademicYear extends Model
 {
     use HasFactory;
+
+    protected $appends = ["academicDays"];
 
     protected $fillable = [
         'name',
@@ -26,5 +29,38 @@ class AcademicYear extends Model
     public function academicBreaks()
     {
         return $this->hasMany(AcademicBreak::class, 'academic_year_id', 'id');
+    }
+
+    function getAcademicDaysAttribute() {  
+        $academicBreaks = $this->academicBreaks;
+
+        $startDate = Carbon::parse($this->from);
+        $endDate = Carbon::parse($this->to);   
+
+        $totalDays = 0;
+        $conflictedDays = [];
+
+        foreach ($academicBreaks as $academicBreak) {
+            $academicBreakFrom = Carbon::parse($academicBreak->from);
+            $academicBreakTo = Carbon::parse($academicBreak->to);
+
+            for ($date = $academicBreakFrom; $date->lte($academicBreakTo); $date->addDay()) {
+                $dateString = $date->toDateString();
+
+                if (!in_array($dateString, $conflictedDays)) {
+                    $conflictedDays[] = $dateString;
+                }
+            }
+        }
+
+        for ($date = $startDate; $date->lte($endDate); $date->addDay()) {
+            $currentDayOfWeek = $date->dayOfWeek;
+
+            if (!in_array($currentDayOfWeek, [5, 6]) && !in_array($date->toDateString(), $conflictedDays)) {
+                $totalDays++;
+            }
+        }
+
+        return $totalDays;
     }
 }
