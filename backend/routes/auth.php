ed95a2e9959c87ebba2d7887cc7d3b6752e1ab40
Ilyes Azzi <ilyes24.azzi@gmail.com>
2023-09-20 22:15:40 +0200

    Backend - Routes splitted into files

--- backend/routes/auth.php
+++ backend/routes/auth.php
@@ -4,7 +4,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Database\Eloquent\ModelNotFoundException;
 
-Route::group(['prefix' => 'auth', 'namespace' => 'App\Http\Controllers\Api', 'middleware' => ['convert.bool.string']], function(){  
+Route::group(['prefix' => 'auth', 'namespace' => 'App\Http\Controllers\Auth', 'middleware' => ['convert.bool.string']], function(){  
     Route::post('register', 'AuthController@register');
     Route::post('login', 'AuthController@login');
     Route::post('password/email', 'AuthController@sendResetLinkEmail');
