96f5fd7d4069fa656440e6fb286ae138e544cf0e
Ilyes Azzi <ilyes24.azzi@gmail.com>
2023-11-04 18:37:09 +0100

    Frontend - Pagination issue solved

--- frontend/src/lib/components/Pagination.svelte
+++ frontend/src/lib/components/Pagination.svelte
@@ -20,14 +20,14 @@
 </script>
 
 
-<div class="row mt-4 align-items-center">
+<div class="row mt-4 align-items-center flex-wrap">
     <div class="col-sm-12 col-md-6">
         <p >{translation.showing[localStorage.getItem("language")]} <span class="fw-bold">{from}</span> {translation.to[localStorage.getItem("language")]} <span class="fw-bold">{to}</span> {translation.of[localStorage.getItem("language")]} <span class="fw-bold">{total}</span> {translation.results[localStorage.getItem("language")]}</p>
     </div>
     <div class="col-sm-12 col-md-6  ">
     <nav>
-        <ul class="pagination justify-content-md-end justify-content-sm-start pagination-separated">
-            <li class="page-item" class:disabled={current_page == 1}><a class="page-link" on:click={() => switchPage(current_page - 1)} href="#">{translation.previous[localStorage.getItem("language")]}</a></li>
+        <ul class="pagination justify-content-md-end justify-content-sm-start pagination-separated pagination-sm">
+            <li class="page-item" class:disabled={current_page == 1}><a class="page-link ms-0" on:click={() => switchPage(current_page - 1)} href="#">{translation.previous[localStorage.getItem("language")]}</a></li>
             {#each pages as page,i}
                 <li class="page-item" class:active={current_page == i+1}><a class="page-link" on:click={() => switchPage(i+1)} href="#">{page}</a></li>
             {/each}
