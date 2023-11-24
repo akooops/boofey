88723143c1031d1acff3a625898be311af402e06
younlok <younlok23@gmail.com>
2023-11-24 15:14:40 +0100

    frontend - saved invoices from death admin side

---
+++ frontend/src/routes/(app)/admin/invoices/+page.js
@@ -0,0 +1,17 @@
+import { PathGetInvoices,DefaultGetQueries } from "$lib/api/paths"
+import { redirector } from "$lib/api/auth";
+
+export const ssr = false;
+export async function load({fetch,url,depends}) {
+    depends('invoices:refresh');
+    let res = await fetch(PathGetInvoices(DefaultGetQueries(url)),{
+        headers:{
+            Authorization: `${localStorage.getItem("SID")}`
+        }
+    })
+    redirector(res)
+
+
+    let invoicesResponse = await res.json() 
+    return {invoicesResponse,tabTitle:"Invoices"}
+};
---
+++ frontend/src/routes/(app)/admin/invoices/+page.svelte
@@ -0,0 +1,47 @@
+<script>
+    import Pagination from "$lib/components/Pagination.svelte";
+    import SearchTable from "$lib/components/SearchTable.svelte";
+    
+    import InvoicesTable from "$lib/tables/InvoicesTable.svelte";
+    import { onMount } from "svelte";
+import { fade } from 'svelte/transition';
+    import {initToolTip} from "$lib/init/initToolTip.js"
+    export let data
+    
+    $: invoicesList = data.invoicesResponse.data.invoices  
+    $: invoicesPagination = data.invoicesResponse.pagination
+    let invoicesPage
+    onMount(() => {
+        initToolTip(invoicesPage)
+    })
+    
+    </script>
+    <div class="row"  in:fade={{duration: 200 }} bind:this={invoicesPage}>
+        <div class="col-lg-12">
+            <div class="card">
+                <div class="card-header align-items-center d-flex">
+                    <h4 class="card-title mb-0 flex-grow-1">Invoices Managment</h4>
+                    <div class="flex-shrink-0">
+                    </div>
+                </div><!-- end card header -->
+                {#if JSON.parse(sessionStorage.getItem("permissions")).includes("invoices.index")}
+                <div class="card-body">
+    
+                    <!-- <div class="live-preview"> -->
+                        <div class="row">
+                                <!-- Input with Icon -->
+                            <SearchTable type={"Invoice"}/>
+                            <InvoicesTable {invoicesList}/>
+                            <Pagination {...invoicesPagination} />
+                            <!--end col-->
+                        </div>
+                        <!--end row-->
+                    <!-- </div> -->
+                </div><!-- end card-body -->
+                {/if}
+            </div><!-- end card -->
+        </div>
+        <!-- end col -->
+    </div>
+    
+    
