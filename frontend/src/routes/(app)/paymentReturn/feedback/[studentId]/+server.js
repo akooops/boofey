7553f3e1a7174c947de3d3cfbf93a655964b3cec
younlok <younlok23@gmail.com>
2023-12-03 21:02:01 +0100

    frontend - payment sys

--- frontend/src/routes/(app)/paymentReturn/feedback/[studentId]/+server.js
+++ frontend/src/routes/(app)/paymentReturn/feedback/[studentId]/+server.js
@@ -4,20 +4,6 @@ import {PathAddPaymentMethod,PathCheckPaymentRedirection} from "$lib/api/paths.j
 export async function POST({ request, cookies,url,params }) {
 	let values = await request.formData();
    
-    let formData = new FormData()
-    for (let pair of values.entries()) {
-        formData.set(pair[0],pair[1]); 
-    }
 
-    console.log("feedback")
-
-    let res = await fetch(PathCheckPaymentRedirection(values.get("merchant_reference")),{
-        method:"POST",
-        // body:formData
-    })
-    res = await res.json()
-    console.log(res)
-    let msg = res?.error?.message == null ? "" : `&msg=${res?.error?.message}`
-    console.log(`/students/${params.studentId}/subscriptions?status=${res.status}${msg}`)
-	throw redirect(303, `/students/${params.studentId}/subscriptions?status=${res.status}${msg}`);
+	throw redirect(303, `/students/${params.studentId}/subscriptions?ref=${values.get("merchant_reference")}`);
 }
