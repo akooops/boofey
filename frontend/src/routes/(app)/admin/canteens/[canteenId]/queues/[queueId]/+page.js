b4a6d1ace357a2ca9d6762ab12427f75bdca180d
younlok <younlok23@gmail.com>
2023-12-15 14:25:53 +0100

    frontend - update

---
+++ frontend/src/routes/(app)/admin/canteens/[canteenId]/queues/[queueId]/+page.js
@@ -0,0 +1,41 @@
+import { PathGetQueues,DefaultGetQueries,PathGetQueueStudents } from "$lib/api/paths"
+import { redirector } from "$lib/api/auth";
+
+export const ssr = false;
+export async function load({fetch,url,depends,params}) {
+    depends('queues:refresh');
+    let res = await fetch(PathGetQueues(params.canteenId,DefaultGetQueries(url)),{
+        headers:{
+            Authorization: `${localStorage.getItem("SID")}`
+        }
+    })
+    redirector(res)
+
+
+    let queuesResponse = await res.json()
+    
+    let activeQueue = queuesResponse.data.activeQueue
+    let activeQueueStudents
+    let currentQueue
+    if(activeQueue){
+        res = await fetch(PathGetQueueStudents(params.queueId,DefaultGetQueries(url),true),{
+            headers:{
+                Authorization: `${localStorage.getItem("SID")}`
+            }
+        })
+        redirector(res)
+        
+        let ActiveQueuesResponse = await res.json()
+
+        activeQueueStudents = ActiveQueuesResponse.data.queueStudents
+        currentQueue = ActiveQueuesResponse.data.queue
+
+
+    }
+
+
+
+    
+
+    return {queuesResponse,currentQueue,activeQueueStudents,tabTitle:"Queues"}
+};
---
+++ frontend/src/routes/(app)/admin/canteens/[canteenId]/queues/[queueId]/simple/ActiveQueueStudentCard.svelte
@@ -0,0 +1,44 @@
+<script>
+    import { getContext } from "svelte"
+    import {translation} from "$lib/translation.js"
+    import { goto } from '$app/navigation';
+
+    export let queueStudent
+
+    let {queueStudentStore} = getContext("queueStudentStore")
+    
+    function setQueueStudent(){
+        $queueStudentStore = JSON.parse(JSON.stringify(queueStudent));
+    }
+
+
+
+
+</script>
+
+
+<div class="col-xxl-3 col-xl-4  col-lg-5  col-sm-6   col-xs-12">
+    <div class="card card-animate p-3 py-2"   role="button">
+        <div class="row g-0">
+                <div class="card-body text-center">
+                    <div class="avatar-md mb-3 mx-auto">
+                        <img src={queueStudent.student.image.full_path} alt="" id="candidate-img" class="avatar-md rounded-circle">
+                    </div>
+                    <p class="text-muted mb-0 fs-6">#{queueStudent.id}</p>
+                    <h5 id="candidate-name" class="mb-1 text-truncate">{queueStudent.student.fullname}</h5>
+                    <p class="text-muted mb-0 fs-6">Started at : {queueStudent.started_at}</p>
+                    <div class="d-flex gap-2  mt-4 flex-wrap">
+                        {#if JSON.parse(sessionStorage.getItem("permissions")).includes("queueStudents.exit.simplified")}
+                            <span  data-bs-toggle="modal" class="col-12" data-bs-target="#exitQueueStudent" on:click|stopPropagation={setQueueStudent} >
+                                <button type="button" class="btn  col-12 text-start btn-soft-success btn-label  waves-effect waves-light">
+                                    <i class="ri-logout-box-r-line label-icon align-middle fs-16 me-2"></i> Exit Queue
+                                </button>
+                            </span>
+                        {/if}
+                      
+                    </div>
+                </div>
+        </div>
+    </div>
+    <!-- end card -->
+</div>
