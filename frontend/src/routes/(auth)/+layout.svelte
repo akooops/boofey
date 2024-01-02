202b7f1aba853bda830445961b6485637cbd1a84
younlok <younlok23@gmail.com>
2024-01-02 14:09:32 +0100

    logo href

--- frontend/src/routes/(auth)/+layout.svelte
+++ frontend/src/routes/(auth)/+layout.svelte
@@ -26,7 +26,7 @@
                 <div class="col-lg-12">
                     <div class="text-center mt-sm-5 mb-4 text-white-50">
                         <div>
-                            <a href="index.html" class="d-inline-block auth-logo">
+                            <a href="/" class="d-inline-block auth-logo">
                                 <img src="/assets/images/logo-white.svg" alt="" height="30">
                             </a>
                         </div>
