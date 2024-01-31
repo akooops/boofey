6ef9e6a6f03cc74d5651fa02184e9de8e4efa0fa
Ilyes Azzi <ilyes24.azzi@gmail.com>
2024-01-31 13:30:11 +0100

    Backend - Rate limiting updated

--- backend/routes/sync.php
+++ backend/routes/sync.php
@@ -4,7 +4,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Database\Eloquent\ModelNotFoundException;
 
-Route::group(['prefix' => 'sync', 'namespace' => 'App\Http\Controllers\Sync', 'middleware' => ['verify.apikey']], function() {
+Route::group(['prefix' => 'sync', 'namespace' => 'App\Http\Controllers\Sync', 'middleware' => ['verify.apikey', 'throttle:1000,1']], function() {
     Route::get('/', 'SyncController@sync')->name('sync');
     Route::get('/students', 'SyncController@syncStudents')->name('students.sync');
     Route::post('/queues', 'SyncController@queues')->name('queues.sync');
